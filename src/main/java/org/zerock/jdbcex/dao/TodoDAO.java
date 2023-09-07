828632f7f1fd577b2ce48f36eeef3e3b073f6dbb
PARKEUNGYEONG <eungyeong1140@naver.com>
2023-09-07 16:30:17 +0900

    Changes

--- src/main/java/org/zerock/jdbcex/dao/TodoDAO.java
+++ src/main/java/org/zerock/jdbcex/dao/TodoDAO.java
@@ -1,5 +1,7 @@
 package org.zerock.jdbcex.dao;
 
+import lombok.Cleanup;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -22,4 +24,22 @@ public class TodoDAO {
         }
         return now;
     }
+
+    public String getTIme2() throws Exception{
+
+        /*@Cleanup 을 이용하면 Lombok 라이브러리에 상당히 종송적인 코드를 작성하게 된다는 부담이 있기는 하지만
+        * 최소한의 코드로 close()가 보장되는 코드를 작성할 수 있다는 장점이 있다.*/
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement("select now");
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        String now = resultSet.getString(1);
+
+        return now;
+    }
+
 }
+
+/*getTime()은 try-with-resources 기능을 이용해서 try()내에 선언된 변수들이 자동으로 close()될 수 있는 구조로 작성*/
---
+++ src/test/java/org/zerock/dao/TodoDAOTests.java
@@ -0,0 +1,25 @@
+/*DAO를 작성하면 항상 테스트 코드를 이용해서 동작에 문제가 없는지를 확인하는것이 좋다*/
+
+package org.zerock.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.zerock.jdbcex.dao.TodoDAO;
+
+public class TodoDAOTests {
+
+    private TodoDAO todoDAO;
+
+    @BeforeEach
+    public void ready(){
+        todoDAO = new TodoDAO();
+    }
+
+    @Test
+    public void testTime() throws Exception{
+        System.out.println(todoDAO.getTime());
+    }
+}
+
+/*TodoDATOTests 는 @BeforeEach를 이용하는 ready()를 통해서 모든 테스트전에 TodoDAO타입의 객체를 생성하도록 하고, testTime()을 이용해서
+* TodoDAO에 작성한 getTime()이 정상 작동하는지를 확인한다.*/
